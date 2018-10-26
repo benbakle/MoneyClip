05e2d310a94f2686d16e9b1ddb9a7bf251dabc18
Benjamin Bakle <bbakle@apterainc.com>
2018-10-25 21:16:39 -0400

    feat: add migrations for ef core

---
+++ MoneyClip/ApplicationDataContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyClip.EntityFramework
+{
+    public class ApplicationDataContext : DbContext
+    {
+        public ApplicationDataContext(DbContextOptions<ApplicationDataContext> options) : base(options)
+        {
+        }
+
+    }
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDataContext>
+    {
+        public ApplicationDataContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<ApplicationDataContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectionString);
+            return new ApplicationDataContext(builder.Options);
+        }
+    }
+}
---
+++ MoneyClip/Models/Income.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyClip.Models
+{
+    public class Income
+    {
+        public int IncomeID { get; set; }
+        public string Description { get; set; }
+    }
+}
