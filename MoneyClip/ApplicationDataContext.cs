c887c57366ff33ce5698bb10fd1f94607d767c89
Benjamin Bakle <bbakle@apterainc.com>
2018-10-27 00:07:14 -0400

    maint: moved Models to an external project

--- MoneyClip/ApplicationDataContext.cs
+++ MoneyClip/ApplicationDataContext.cs
@@ -2,11 +2,7 @@
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using MoneyClip.Models;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MoneyClip.EntityFramework
 {
