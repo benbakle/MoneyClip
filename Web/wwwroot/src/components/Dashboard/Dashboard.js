3e0c0479a6dd61a5fe802a5c8753d8eb34a1fc72
Benjamin Bakle <bbakle@apterainc.com>
2018-11-22 00:44:00 -0500

    feat: add income UI

--- Web/wwwroot/src/components/Dashboard/Dashboard.js
+++ Web/wwwroot/src/components/Dashboard/Dashboard.js
@@ -1,13 +1,15 @@
 ﻿import React from 'react';
 import ApiFetch from '../ApiFetch/ApiFetch';
 import Incomes from '../Incomes/Incomes';
+import IncomesCreate from '../Incomes/Create';
 
 export default class Dashboard extends React.Component {
     render() {
         return (
             <div className="dashboard">
                 <div className="mc-container">
-                  <Incomes />
+                    <Incomes />
+                    <IncomesCreate />
                 </div>
             </div>
         );
---
+++ Web/wwwroot/src/components/Incomes/Create.js
@@ -0,0 +1,45 @@
+﻿import React from 'react';
+import ApiFetch from '../ApiFetch/ApiFetch';
+
+export default class IncomesCreate extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            description: "",
+            amount: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(evt) {
+        this.setState({ [evt.target.name]: evt.target.value });
+    }
+
+    render() {
+        return (
+            <div className="incomes board">
+                <div className="mc-container">
+                    <div className="section-header">
+                        <div className="title">
+                            <span>Add</span>
+                        </div>
+                    </div>
+                    <div className="income-content">
+                        <label>Description:</label>
+                        <input type="text" name="description" onChange={this.handleChange} value={this.state.description} />
+                        <div>
+                            {this.state.description}
+                        </div>
+                        <label>Amount:</label>
+                        <input type="text" name="amount" onChange={this.handleChange} value={this.state.amount} />
+                        <div>
+                            {this.state.amount}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
