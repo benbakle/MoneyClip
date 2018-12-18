5f7e29d5006e93145c88b3e4c2dc1609431accbe
Benjamin Bakle <bbakle@apterainc.com>
2018-12-17 23:00:39 -0500

    feat: create income validation

--- Web/wwwroot/src/components/income/Create.js
+++ Web/wwwroot/src/components/income/Create.js
@@ -8,34 +8,61 @@ export default class Create extends React.Component {
         this.state = {
             description: "",
             amount: "",
+            amountIsValid: true
         }
 
-        this.handleChange = this.handleChange.bind(this);
+        this.validateDescription = this.validateDescription.bind(this);
+        this.validateAmount = this.validateAmount.bind(this);
         this.submit = this.submit.bind(this);
     }
 
-    handleChange(evt) {
-        this.setState({ [evt.target.name]: evt.target.value });
+    validateDescription(e) {
+        this.setState({
+            description: e.target.value
+        })
+    }
+
+    validateAmount(e) {
+        (/^\d*\.?\d*$/.test(e.target.value))
+            ? this.setState({ amount: e.target.value, amountIsValid: true })
+            : this.setState({ amountIsValid: false });
     }
 
     submit() {
-        let income = {
-            Description: this.state.description,
-            Amount: this.state.amount
+        if (this.state.description != "" && this.state.amount != "") {
+            let income = {
+                Description: this.state.description,
+                Amount: this.state.amount
+            }
+            Api.create('incomes', income).then(this.props.callback);
         }
-        Api.create('incomes', income).then(this.props.callback);
     }
 
     render() {
         return (
             <div className="create-income">
-                <label>Description:</label>
-                <input type="text" name="description" onChange={this.handleChange} value={this.state.description} />
-
-                <label>Amount:</label>
-                <input type="text" name="amount" onChange={this.handleChange} value={this.state.amount} />
-                <div className="flex flex-end">
-                    <button className="button" onClick={this.submit}>Add</button>
+                <div className="description">
+                    <label>Description:</label>
+                    {
+                        this.state.description == "" &&
+                        <span className="required">*</span>
+                    }
+                    <input type="text" name="description" onChange={this.validateDescription} value={this.state.description} />
+                </div>
+                <div className="amount">
+                    <label>Amount:</label>
+                    {
+                        this.state.amount == "" &&
+                        <span className="required">*</span>
+                    }
+                    <input autoComplete="off" type="text" name="amount" onChange={this.validateAmount} value={this.state.amount} />
+                    {
+                        !this.state.amountIsValid &&
+                        <div className="help">'amount' can only be a number</div>
+                    }
+                    <div className="flex flex-end">
+                        <button className="button" onClick={this.submit}>Add</button>
+                    </div>
                 </div>
             </div>
         );
--- Web/wwwroot/src/components/income/Create.test.js
+++ Web/wwwroot/src/components/income/Create.test.js
@@ -5,18 +5,77 @@ import Api from '../../services/Api';
 import { promise, resolved } from '../../setupTests';
 
 describe("The create incomes component", () => {
-    let _component;
-    describe("given the add income button is clicked", () => {
-        it("calls the api", () => {
-            spyOn(Api, "create").and.returnValue(resolved());
-            _component = shallow(<Create />);
-            _component.setState({
-                description: "this shiz",
-                amount: 69.69
-            });
-            _component.find("button").simulate("click");
+    let component;
+    beforeEach(() => {
+        spyOn(Api, "create").and.returnValue(resolved());
+        component = shallow(<Create />);
+    })
 
-            expect(Api.create).toHaveBeenCalledWith("incomes", { Description: "this shiz", Amount: 69.69 });
+    describe("given the description has changed", () => {
+        it("updates the description state", () => {
+            let e = { target: { value: "Here is a description" } };
+            component.instance().validateDescription(e);
+
+            expect(component.state().description).toEqual("Here is a description");
+        });
+    });
+
+    describe("given the amount changed", () => {
+        describe("given the amount is NOT valid", () => {
+            beforeEach(() => {
+                let e = { target: { value: "a" } };
+                component.instance().validateAmount(e);
+            });
+
+            it("does not update the amount", () => {
+                expect(component.state().amount).toEqual("");
+            });
+
+            it("shows a help message", () => {
+                expect(component.state().amountIsValid).toEqual(false);
+                expect(component.find(".amount .help").text()).toEqual("'amount' can only be a number");
+            })
+
+        });
+
+        describe("given the amount is valid", () => {
+            it("updates the amount", () => {
+                let e = { target: { value: 23.99 } };
+                component.instance().validateAmount(e);
+                expect(component.state().amount).toEqual(23.99);
+            });
+        });
+    });
+
+    describe("given the add income button is clicked", () => {
+        describe("and amount has no value", () => {
+            beforeEach(() => {
+                component.setState({ description: "suck it", amount: "" });
+                component.find("button").simulate("click");
+            });
+
+            it("does not call the api", () => {
+                expect(Api.create).not.toHaveBeenCalled();
+            });
+
+            it("marks amount as required", () => {
+                expect(component.find(".amount .required").text()).toEqual("*");
+            });
+        });
+
+        describe("and description has no value", () => {
+            it("does not call the api", () => {
+                component.setState({ description: "", amount: 6 });
+                component.find("button").simulate("click");
+                expect(Api.create).not.toHaveBeenCalled();
+            })
+        })
+        describe("and there are values", () => {
+            it("calls the api", () => {
+                component.setState({ description: "this shiz", amount: 69.69 });
+                component.find("button").simulate("click");
+                expect(Api.create).toHaveBeenCalledWith("incomes", { Description: "this shiz", Amount: 69.69 });
+            });
         });
     });
 });
