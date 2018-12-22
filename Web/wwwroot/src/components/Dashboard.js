f7eb0e1ea6190d74ec775eecbeee42e23da36925
Benjamin Bakle <bbakle@apterainc.com>
2018-12-21 20:49:15 -0500

    clean: remove StoredValues table

--- Web/wwwroot/src/components/Dashboard.js
+++ Web/wwwroot/src/components/Dashboard.js
@@ -2,6 +2,7 @@
 import Incomes from './income/Incomes';
 import Card from './layouts/BoardCard';
 import StoredValues from './StoredValues';
+import Transaction from './transaction/Transaction';
 
 export default class Dashboard extends React.Component {
     render() {
@@ -9,6 +10,7 @@ export default class Dashboard extends React.Component {
             <div className="dashboard">
                 <div className="mc-container">
                     <Card card={<Incomes />} />
+                    <Card card={<Transaction />} />
                     <Card card={<StoredValues field="bankBalance" label="Balance"  />} />
                 </div>
             </div>
---
+++ Web/wwwroot/src/components/transaction/Transaction.js
@@ -0,0 +1,53 @@
+﻿import React from 'react';
+//import Delete from './Delete';
+//import Update from './Update';
+import Money from '../Money';
+
+export default class Transaction extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            inEditMode: false,
+            transaction :{ description: "tlkj", amount: 53.88, date: "2018-01-01" }
+        }
+
+        this.enterEditMode = this.enterEditMode.bind(this);
+        this.exitEditMode = this.exitEditMode.bind(this);
+    }
+
+    exitEditMode() {
+        this.setState({ inEditMode: false});
+    }
+
+    enterEditMode() {
+        this.setState({ inEditMode: true });
+    }
+
+    render() {
+        return (
+           // this.props.transaction &&
+            <div className="transaction" >
+                <div className="title">Transactions</div>
+                {
+                    !this.state.inEditMode &&
+                    <React.Fragment>
+                        <div className="date">{this.state.transaction.date}</div>
+                        <button className="description link" onClick={this.enterEditMode}>{this.state.transaction.description}</button>
+                        <Money className="amount" value={this.state.transaction.amount} />
+                    </React.Fragment>
+                }
+                {
+                    this.state.inEditMode &&
+                        //<Update transaction={this.props.transaction} callback={this.props.callback} />
+                        //<Delete id={this.props.transaction.id} callback={this.props.callback} />
+                    <React.Fragment>
+                        <div>
+                            <button className="link close" onClick={this.exitEditMode}><i className='far fa-times-circle'></i></button>
+                        </div>
+                    </React.Fragment>
+                }
+            </div>
+        );
+    }
+}
